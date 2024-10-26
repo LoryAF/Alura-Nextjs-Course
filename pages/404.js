60be230c7f32a4eb29858eb60a1d406f9dd9ad22
Lory <lorconcurso@gmail.com>
2024-10-26 17:58:23 -0300

    Commit

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Você se perdeu e caiu na pagina 404 :O</h1>
+            <Link href="/">
+                Ir para a página do Home
+            </Link>
+        </div>
+    )
+}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de Perguntas FAQ</h1>
+            <Link href="/">
+                Ir para a página do Home
+            </Link>
+        </div>
+    )
+}
